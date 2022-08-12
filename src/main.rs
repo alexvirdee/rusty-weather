6f6726b0faef712f089590ccbcdd6add0e808e94
Alexinder Virdee <alex.virdee08@gmail.com>
2022-08-12 10:54:26 -0400

    Setting up weather util tool to pull from openweathermap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use std::io;
 
+// Use `WeatherApi` to use `reqwest` Client and use all of the metadata required to query the openweathermap.org api
+
+#[cfg(feature = "cli")]
+#[derive(Default, Clone)]
+
+pub struct WeatherApi {
+    client: Client,
+    api_key: ApiStringType,
+    api_endpoint: StringType,
+    api_path: StringType
+}
+
 fn main() {
     println!("Hello from rusty-weather");
 
     println!("Please input your local city");
 
-    let mut city = String::new();
+    let mut city = String::new(); // mutable city variable
 
     io::stdin()
         .read_line(&mut city)
-        .expect("Fasiled to read line");
+        .expect("Failed to read line");
 
     println!("The city you entered is: {city}");
 }
